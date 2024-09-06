9075ae925e8e7fc265d34460a30b7642ab493f88
mechsoon <ksw5190@gmail.com>
2024-09-06 13:57:59 +0200

    Finished speedDataSend

---
+++ arduino/speedsensor.c
@@ -0,0 +1,84 @@
+#include <mcp_can.h>
+#include <SPI.h>
+#include <stdint.h>
+
+const int SPI_CS_PIN = 9;
+int encoder_pin = 3;  // The pin the encoder is connected           
+unsigned int rpm;     // rpm reading
+volatile byte pulses;  // number of pulses
+unsigned long timeold; 
+unsigned int pulsesperturn = 20;
+
+MCP_CAN CAN(SPI_CS_PIN);                                    // Set CS pin
+
+void counter()
+ {
+    //Update count
+      pulses++;    
+ }
+
+void setup()
+{
+    Serial.begin(115200);
+
+    while (CAN_OK != CAN.begin(MCP_ANY, CAN_500KBPS, MCP_16MHZ))
+    //while (CAN_OK != CAN.begin(CAN_500KBPS))              // init can bus : baudrate = 500k
+    {
+        Serial.println("CAN BUS Shield init fail");
+        Serial.println(" Init CAN BUS Shield again");
+        delay(100);
+    }
+    Serial.println("CAN BUS Shield init ok!");
+    CAN.setMode(MCP_NORMAL);
+
+    pinMode(encoder_pin, INPUT);
+    attachInterrupt(digitalPinToInterrupt(encoder_pin), counter, FALLING);
+    pulses = 0;
+    rpm = 0;
+    timeold = 0;
+
+}
+
+// //unsigned char stmp[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+// unsigned char stmp[8] = {0};  // CAN 메시지 데이터 배열 초기화
+
+// int 값을 바이트로 변환하여 stmp 배열에 저장
+
+void loop() {
+    if (millis() - timeold >= 1000){  /*Uptade every one second, this will be equal to reading frecuency (Hz).*/
+ 
+    //Don't process interrupts during calculations
+    detachInterrupt(0);
+    //Note that this would be 60*1000/(millis() - timeold)*pulses if the interrupt
+    //happened once per revolution
+    rpm = (60 * 1000 / pulsesperturn )/ (millis() - timeold)* pulses;
+    timeold = millis();
+    pulses = 0;
+   
+    //Write it out to serial port
+    Serial.print("RPM = ");
+    Serial.println(rpm,DEC);
+    //Restart the interrupt processing
+    attachInterrupt(0, counter, FALLING);
+    }
+
+    //unsigned char stmp[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+    unsigned char stmp[8] = {0};  // CAN 메시지 데이터 배열 초기화
+    stmp[0] = (rpm >> 24) & 0xFF;  // 상위 8비트
+    stmp[1] = (rpm >> 16) & 0xFF;
+    stmp[2] = (rpm >> 8) & 0xFF;
+    stmp[3] = rpm & 0xFF; 
+
+
+    stmp[7]++;  // 데이터 증가
+
+    // CAN 메시지 전송: ID 0x100, 데이터 길이 8, stmp 데이터
+    if (CAN_OK == CAN.sendMsgBuf(0x100, 0, 8, stmp)) {
+        //Serial.println("Message Sent Successfully!");  // 성공 여부 확인
+    } else {
+        Serial.println("Error Sending Message");
+    }
+
+    delay(100);  // 100ms마다 전송
+}
+// END FILE
