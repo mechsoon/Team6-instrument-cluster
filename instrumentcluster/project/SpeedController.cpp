912835bbd1c3b32686fa7c98251c388a1dedc22d
mechsoon <ksw5190@gmail.com>
2024-09-11 11:34:23 +0200

    update qtimer code

--- instrumentcluster/project/SpeedController.cpp
+++ instrumentcluster/project/SpeedController.cpp
@@ -1,17 +1,22 @@
 #include "SpeedController.h"
 
 SpeedController::SpeedController(QObject *parent)
-    : QObject(parent), m_speed(0)  // 초기 속도는 0
+    : QObject(parent), m_speed(0) 
 {
     setupCanInterface();
+
+    connect(timer, &QTimer::timeout, this, &SpeedController::updateSpeed);
+    timer->start(100);  
 }
 
 SpeedController::~SpeedController() {
-    // 소켓 종료 등 필요한 정리 작업
+    if (canSocket >= 0) {
+        close(canSocket);
+    }
 }
 
 double SpeedController::speed() const {
-    return m_speed;  // QML에서 속도 값을 읽기 위한 함수
+    return m_speed;  
 }
 
 void SpeedController::setupCanInterface()
@@ -19,14 +24,12 @@ void SpeedController::setupCanInterface()
     struct ifreq ifr;
     struct sockaddr_can addr;
 
-    // 소켓 생성
     canSocket = socket(PF_CAN, SOCK_RAW, CAN_RAW);
     if (canSocket < 0) {
         std::cerr << "Error while opening socket" << std::endl;
         return;
     }
 
-    // 네트워크 인터페이스 설정
     std::strcpy(ifr.ifr_name, "can0");
     ioctl(canSocket, SIOCGIFINDEX, &ifr);
 
@@ -34,7 +37,6 @@ void SpeedController::setupCanInterface()
     addr.can_family = AF_CAN;
     addr.can_ifindex = ifr.ifr_ifindex;
 
-    // 소켓 바인딩
     if (bind(canSocket, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
         std::cerr << "Error in socket bind" << std::endl;
         return;
@@ -47,14 +49,12 @@ void SpeedController::updateSpeed()
     int nbytes = read(canSocket, &frame, sizeof(struct can_frame));
 
     if (nbytes > 0 && frame.can_id == 0x100) {
-        // CAN 메시지에서 속도 값 추출
-        int speed = (frame.data[0] << 8) | frame.data[1];
-        double actualSpeed = speed / 10.0;  // 소수점 속도 처리
 
-        // 속도 값이 변경되면 QML로 알림
-        if (m_speed != actualSpeed) {
-            m_speed = actualSpeed;
-            emit speedChanged();  // 속도 변경 시 QML에 알림
+        int speed = frame.data[0]
+
+        if (m_speed != speed) {
+            m_speed = speed;
+            emit speedChanged();  
         }
     }
 }
--- instrumentcluster/project/SpeedController.h
+++ instrumentcluster/project/SpeedController.h
@@ -8,29 +8,29 @@
 #include <net/if.h>
 #include <cstring>
 #include <iostream>
+#include <QTimer> 
 
 class SpeedController : public QObject
 {
     Q_OBJECT
-    Q_PROPERTY(double speed READ speed NOTIFY speedChanged)  // QML과 상호작용할 속성
+    Q_PROPERTY(double speed READ speed NOTIFY speedChanged)
 
 public:
     explicit SpeedController(QObject *parent = nullptr);
     ~SpeedController();
 
-    double speed() const;   // QML에서 속도 값을 읽기 위한 getter 함수
+    double speed() const; 
 
 signals:
-    void speedChanged();   // QML에 속도 값이 변경되었음을 알리는 시그널
+    void speedChanged();  
 
 public slots:
-    void updateSpeed();  // CAN에서 속도를 받아 업데이트하는 함수
-
+    void updateSpeed(); 
 private:
     int canSocket;
-    double m_speed;  // 속도를 저장하는 멤버 변수
-
-    void setupCanInterface();  // CAN 인터페이스 설정 함수
+    double m_speed;  
+    QTimer *timer;  
+    void setupCanInterface(); 
 };
 
 #endif // SPEEDCONTROLLER_H
